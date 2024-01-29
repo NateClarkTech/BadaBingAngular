5447b5898e64d0c40da43b4b59cf89a5e17b4f97
NateClarkTech <NateClarkTech@gmail.com>
2024-01-29 08:42:53 -0500

    Working on order page

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, inject } from '@angular/core';
 import { MENU } from '../menu/menuContents';
 import { MenuSection } from '../menu/menuSection';
 import { MenuItem } from '../menu/menuItem';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule } from '@angular/forms';
 import { ToppingListComponent } from './topping-list/topping-list.component';
 @Component({
   selector: 'app-order',
@@ -16,6 +16,7 @@ export class OrderComponent{
   menu: MenuSection[] = MENU;
   orderTotal: number = 0;
   orderContents: MenuItem[] = []
+  selectedPizza: string = ''; //A state of the selected pizza.
 
   addToOrder(item: MenuItem): void{
     this.orderTotal += item.itemPrice
@@ -33,16 +34,49 @@ export class OrderComponent{
     });
   } 
 
-  addToOrderPizza(item: MenuItem, numToppings: number){
+  addToOrderPizza(item: MenuItem, numToppings: number)  {
     switch(item.itemName) {
       
     }
   }
 
-  removeFromOrder(index: number){
+  removeFromOrder(index: number): void {
     if (this.orderContents.length !== 0){
       let removedItem: MenuItem[] = this.orderContents.splice(index, 1);
       this.orderTotal -= removedItem[0].itemPrice;
     }
+  }
+
+  changeSelectedPizza(selectedPizza: string): void {
+    switch (selectedPizza){
+      case "One Slice":
+        this.selectedPizza = "One Slice";
+        break;
+
+      case 'Medium (16")':
+        this.selectedPizza = 'Medium (16")';
+        break;
+
+      case 'Large (18")':
+        this.selectedPizza = 'Large (18")';
+        break;
+
+      case "Sicilian (Square)":
+        this.selectedPizza = "Sicilian (Square)";
+        break;
+
+      case "Sheet Pizza (24 Slices)":
+        this.selectedPizza = "Sheet Pizza (24 Slices)";
+        break;
+
+      default: //fallback case, shouldn't occur unless someone adds a new type of pizza without updating this function.
+               //This should be redesigned at some point to make it more maintainable and expandable.
+        console.log("ERROR: INVALID PIZZA TYPE SELECTED - order page - changeSelectedPizza")
+        this.selectedPizza = "";
+      }
+  } 
+
+  clearCheckboxInput(){
+
   }
 }
--- src/app/order/topping-list/topping-list.component.ts
+++ src/app/order/topping-list/topping-list.component.ts
@@ -2,17 +2,28 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuSection } from '../../menu/menuSection';
 import { MENU } from '../../menu/menuContents';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { OrderComponent } from '../order.component';
 
 @Component({
   selector: 'app-topping-list',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './topping-list.component.html',
-  styleUrl: './topping-list.component.css'
+  styleUrl: './topping-list.component.css',
+  providers: [OrderComponent]
 })
 export class ToppingListComponent {
   menu = MENU;
   selectedToppings: string[] = [];
-  formGroup = FormsModule;
+
+  //https://stackoverflow.com/questions/71522120/get-values-of-all-selected-checkboxes-using-angular
+  onChange(topping: string){
+    if (this.selectedToppings.includes(topping)) {
+      this.selectedToppings = this.selectedToppings.filter((item) => item !== topping);
+    } else {
+      this.selectedToppings.push(topping);
+    }
+    console.log(this.selectedToppings);
+  }
 }
